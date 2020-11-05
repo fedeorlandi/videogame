bf02472c7f9df7c39d8a3dbce8cfcca10bcd8afe
fedeorlandi <fedeorlandi4@gmail.com>
2020-11-05 12:18:51 -0300

    using the keyboard

--- videogame.js
+++ videogame.js
@@ -1,7 +1,15 @@
-var canvas = null,
-    ctx = null;
-var x = 50,
-    y = 50;
+var KEY_ENTER = 13,
+    KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40,
+    canvas = null,
+    ctx = null,
+    lastPress = null,
+    pause = true,
+    x = 50,
+    y = 50,
+    dir = 0;
 
 window.requestAnimationFrame = (function () {
     return window.requestAnimationFrame ||
@@ -12,28 +20,95 @@ window.requestAnimationFrame = (function () {
         };
 }());
 
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.which;
+}, false);
+
 function paint(ctx) {
+    // Clean canvas
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Draw square
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x, y, 10, 10);
+    // Debug last key pressed
+    ctx.fillStyle = '#fff';
+    //ctx.fillText('Last Press: ' + lastPress, 0, 20);
+    // Draw pause
+    if (pause) {
+        ctx.textAlign = 'center';
+        ctx.fillText('PAUSE', 150, 75);
+        ctx.textAlign = 'left';
+    }
+}
+
+function act() {
+    if (!pause) {
+        // Change Direction
+        if (lastPress == KEY_UP) {
+            dir = 0;
+        }
+        if (lastPress == KEY_RIGHT) {
+            dir = 1;
+        }
+        if (lastPress == KEY_DOWN) {
+            dir = 2;
+        }
+        if (lastPress == KEY_LEFT) {
+            dir = 3;
+        }
+
+        // Move Rect
+        if (dir == 0) {
+            y -= 10;
+        }
+        if (dir == 1) {
+            x += 10;
+        }
+        if (dir == 2) {
+            y += 10;
+        }
+        if (dir == 3) {
+            x -= 10;
+        }
+        // Out Screen
+        if (x > canvas.width) {
+            x = 0;
+        }
+        if (y > canvas.height) {
+            y = 0;
+        }
+        if (x < 0) {
+            x = canvas.width;
+        }
+        if (y < 0) {
+            y = canvas.height;  
+        }
+    }
+    // Pause/Unpause
+    if (lastPress == KEY_ENTER) {
+        pause = !pause;
+        lastPress = null;
+    }
+}
+
+function repaint() {
+    window.requestAnimationFrame(repaint);
+    paint(ctx);
+}
+
+function run() {
+    setTimeout(run, 50);
+    act();
 }
 
 function init() {
+    // Get canvas and context
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    // Start game
     run();
-} 
-window.addEventListener('load', init, false);
+    repaint();
+}
 
-function run() {
-    window.requestAnimationFrame(run);
-    act();
-    paint(ctx);
-}
-function act(){
-    x += 2;
-    if (x > canvas.width) {
-        x = 0;
-    }
-}
+window.addEventListener('load', init, false);
